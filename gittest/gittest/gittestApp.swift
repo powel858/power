07fc2bab3f8b5b20e4d48ff8b8ea722130b97f16
powel858 <shipark25@pos.idserve.net>
2025-04-10 19:57:05 +0900

    프로젝트 생성

---
+++ gittest/gittest/gittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestApp.swift
+//  gittest
+//
+//  Created by Powel on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct gittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gittest/gittestTests/gittestTests.swift
@@ -0,0 +1,17 @@
+//
+//  gittestTests.swift
+//  gittestTests
+//
+//  Created by Powel on 4/10/25.
+//
+
+import Testing
+@testable import gittest
+
+struct gittestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
